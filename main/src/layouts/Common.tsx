68b18fd4a907ca65428ede42dccc55f207ad396c
JehMeh <117583938+jamaybyrone@users.noreply.github.com>
2024-10-31 09:02:00 +0000

    chore: remove suspense (#3)
    - legacy from my site
    - also fix tests

--- main/src/layouts/Common.tsx
+++ main/src/layouts/Common.tsx
@@ -1,36 +1,33 @@
 'use client'
 
-import { ReactNode, Suspense } from 'react'
-import Loader from '@/components/loader'
+import { ReactNode } from 'react'
 import Stack from '@mui/material/Stack'
 
 export default function CommonLayout({
   children
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <Suspense fallback={<Loader override={true} />}>
       <Stack
-        direction="column"
-        component="section"
-        sx={[
-          {
-            justifyContent: 'space-between',
-            height: { xs: 'auto', md: '100%' }
-          }
-        ]}
+          direction="column"
+          component="section"
+          sx={[
+              {
+                  justifyContent: 'space-between',
+                  height: { xs: 'auto', md: '100%' }
+              }
+          ]}
       >
-        <Stack
-          direction={{ xs: 'column-reverse', md: 'row' }}
-          sx={{
-            justifyContent: 'center',
-            gap: { xs: 6, sm: 12 },
-            p: { xs: 2, sm: 4 },
-            m: 'auto'
-          }}
-        >
-          {children}
-        </Stack>
+          <Stack
+              direction={{ xs: 'column-reverse', md: 'row' }}
+              sx={{
+                  justifyContent: 'center',
+                  gap: { xs: 6, sm: 12 },
+                  p: { xs: 2, sm: 4 },
+                  m: 'auto'
+              }}
+          >
+              {children}
+          </Stack>
       </Stack>
-    </Suspense>
   )
 }
